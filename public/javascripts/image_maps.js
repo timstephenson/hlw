c4da47e5abe3589438c04e6f08d6678ed6943954
Tim Stephenson <tim@raddonline.com>
2012-03-27 10:45:09 -0700

    Created a third layer.

--- public/javascripts/image_maps.js
+++ public/javascripts/image_maps.js
@@ -6,7 +6,7 @@ function map_init() {
   map = new OpenLayers.Map('map', {});
   
   var summary_1 = new OpenLayers.Layer.Image(
-    'Visual Summary', 
+    ' Summary', 
     '/img/summary_1.jpg',
     new OpenLayers.Bounds(-293.4, -180, 293.4, 180),
     new OpenLayers.Size(5397, 3296),
@@ -14,8 +14,22 @@ function map_init() {
   );
   
   var summary_2 = new OpenLayers.Layer.Image(
-    'Visual Summary Annotations', 
-    '/img/summary_2.jpg',
+    ' Details', 
+    '/img/summary_details.png',
+    new OpenLayers.Bounds(-293.4, -180, 293.4, 180),
+    new OpenLayers.Size(5397, 3296),
+    {
+      numZoomLevels:7, 
+      maxResolution:.625,
+      isBaseLayer:false,
+      visibility: false,
+      opacity: 0.8
+    }
+  );
+  
+  var summary_3 = new OpenLayers.Layer.Image(
+    ' Annotations', 
+    '/img/summary_annotations.png',
     new OpenLayers.Bounds(-293.4, -180, 293.4, 180),
     new OpenLayers.Size(5397, 3296),
     {
@@ -28,7 +42,7 @@ function map_init() {
   );
   
   
-  map.addLayers([summary_1, summary_2]);
+  map.addLayers([summary_1, summary_2, summary_3]);
   
   map.addControl(new OpenLayers.Control.LayerSwitcher({}));
   
